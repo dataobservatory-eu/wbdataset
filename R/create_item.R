9ddf8baab595ac4386937868e001009a2e76f4f1
antaldaniel <antaldaniel@gmail.com>
2025-02-12 09:41:24 +0100

    rename functions, new start vignette

--- R/create_item.R
+++ R/create_item.R
@@ -1,24 +1,22 @@
 #' @title Create a property
-#' @description Creates a property entity on a Wikibase instance with a single
+#' @description Creates an item entity on a Wikibase instance with a single
 #' language labelling and description. New labels and descriptions can be added
 #' in further languages later. This is a wrapper for
 #' \href{https://www.wikidata.org/w/api.php?action=help&modules=wbeditentity}{MediaWiki
 #' action=wbeditentity}
-#' @details Optionally, the function adds an equivalent property to this newly
-#' created property.
-#' @param language A single label, for example,  \code{"duration"}.
-#' @param description A single description, for example,  \code{"Length in
-#'   time."}.
+#' @details Optionally, the function adds an equivalent item to this newly
+#' created item.
+#' @param language A single label, for example, \code{"human"}.
+#' @param description A single description, for example, \code{"A member of the
+#' homo sapience species."}.
 #' @param language A single language code, for example, \code{"en"}.
-#' @param datatype A single character string defining the datatype of this
-#'   property, for example, \code{"quantity".}
 #' @param equivalence_property An optional PID of a property already defined in
 #'   the same Wikibase instance that records the equivalence of this new
-#'   property with a property defined elsewhere, for example, on Wikidata or
+#'   item with a property defined elsewhere, for example, on Wikidata or
 #'   CIDOC-CRM. Defaults to \code{NA_character_}; if left missing, no
 #'   equivalence relations is will be claimed.
-#' @param equivalence_id An optional PID or other identifier in Wikibase that
-#'   records the equivalence of the property. An external ID. Defaults to
+#' @param equivalence_id The identifier that uniquely identifies this item among
+#' another system's defintions. Defaults to
 #'   \code{NA_character_}; if left missing, no equivalence relations is will be
 #'   claimed.
 #' @param wikibase_api_url For example,
@@ -35,15 +33,15 @@
 #'  \item{"id_on_target"}{ The new Property Identifier (PID) on the targeted Wikibase.}
 #'  \item{"label"}{ The propery label}
 #'  \item{"description"}{ The description label}
-#'  \item{"language"}{ The language code of the label.}
-#'  \item{"datatype"}{ The datatype of the property, for example, `string`}
-#'  \item{"wikibase_api_url"}{ The MediaWiki API URL where the new property is created.}
-#'  \item{"equivalence_property"}{ The PID that connects an equivalence ID to the property.}
-#'  \item{"equivalence_id"}{ The ID of an equivalent property defined elsewhere.}
-#'  \item{"success"}{ TRUE if successfully created, FALSE if there was an error.}
-#'  \item{"comment"}{ A summary of the error messages(s), if success is FALSE.}
-#'  \item{"time"}{ The time when the action started.}
-#'  \item{"logfile"}{ The name of the CSV logfile.}
+#'  \item{"language"}{ The language code of the label}
+#'  \item{"datatype"}{ Defaults to `item`}
+#'  \item{"wikibase_api_url"}{ The MediaWiki API URL where the new property is created}
+#'  \item{"equivalence_property"}{ The PID that connects an equivalence ID to the property}
+#'  \item{"equivalence_id"}{ The ID of an equivalent item defined elsewhere}
+#'  \item{"success"}{ TRUE if successfully created, FALSE if there was an error}
+#'  \item{"comment"}{ A summary of the error messages(s), if success is FALSE}
+#'  \item{"time"}{ The time when the action started}
+#'  \item{"logfile"}{ The name of the CSV logfile}
 #' }
 #' @examples
 #' \dontrun{
@@ -54,56 +52,53 @@
 #'    wikibase_api_url = "https://reprexbase.eu/demowiki/api.php"
 #'    )
 #'
-#' create_property(
-#'    label = "duration"
+#' create_item(
+#'    label = "biological object"
 #'    language = "en"
-#'    description = "Length of the process in time"
-#'    datatype = "quantity",
+#'    description = "This class comprises individual items of a material nature,
+#'     which live, have lived or are natural products of or from living organisms.",
+#'    equivalence_property = "P37"  # property for equivalent CIDOC definitions
+#'    equivalence_id = "E20"  # CIDOC-CRM identifier of this concept
 #'    csrf = your_csrf)
 #' }
-#'
+#' @export
 
-create_property <- function(label,
-                            description,
-                            language,
-                            datatype,
-                            equivalence_property = NA_character_,
-                            equivalence_id = NA_character_,
-                            wikibase_api_url,
-                            log_path = tempdir(),
-                            csrf) {
+create_item <- function(label,
+                        description,
+                        language,
+                        equivalence_property = NA_character_,
+                        equivalence_id = NA_character_,
+                        wikibase_api_url,
+                        log_path = tempdir(),
+                        csrf) {
 
   # Save the time of running the code
-  action_time <- action_timestamp_create()
-  log_file_name <- paste0("wbdataset_create_property_", action_timestamp, ".csv")
+  action_timestamp <- action_timestamp_create()
+  log_file_name <- paste0("wbdataset_create_item_", action_timestamp, ".csv")
 
   if ( !is.na(equivalence_id) ) {
-    # If there is an equivalence ID, for example, a PID on Wikidata, than the
+    # If there is an equivalence ID, for example, a QID on Wikidata, than the
     # equivalence property that connects this ID to the newly created property
     # must be given, too.
     assertthat::assert_that(
       ! is.na(equivalence_property),
-      msg = "create_property() cannot add equivalence_id statement without equivalence_property.")
+      msg = "create_item() cannot add equivalence_id statement without equivalence_property.")
   }
 
-  default_labels <- list ( language = language,
-                        value = label)
-  labels_list <- list ( default_labels )
+  default_labels <- list (language = language, value = label)
+  labels_list <- list (default_labels)
   names(labels_list) <- language
 
-  default_descriptions <- list ( language = language,
-                              value = description)
+  default_descriptions <- list (language = language, value = description)
   descriptions_list <- list ( default_descriptions )
   names(descriptions_list) <- language
 
-
-  datastring <- property_identity_datastring_create(
+  datastring <- item_identity_datastring_create(
     labels_list = labels_list,
-    descriptions_list = descriptions_list,
-    datatype = datatype
+    descriptions_list = descriptions_list
   )
 
-  teststring = '{"labels":{"en-gb":{"language":"en-gb","value":"Propertylabel"}},"descriptions":{"en-gb":{"language":"en-gb","value":"Propertydescription"}},"datatype":"string"}'
+  teststring = 'api.php?action=wbeditentity&new=item&data={"labels":{"de":{"language":"de","value":"de-value"},"en":{"language":"en","value":"en-value"}}}'
 
   # Getting the user's CSRF token for writing.
   # See get_csrf, get_csrf_token.
@@ -122,7 +117,7 @@ create_property <- function(label,
     wikibase_api_url,
     body = list(
       action = "wbeditentity",
-      new    = "property",
+      new    = "item",
       data   = datastring,
       token  = csrf_token,
       format = "json"
@@ -132,19 +127,20 @@ create_property <- function(label,
   )
 
   # See if the created POST via wbeditentity was successful
-  created_property_response <- httr::content(new_property,
+  created_item_response <- httr::content(new_property,
                                              as = "parsed",
                                              type = "application/json")
-  created_property_response
+  created_item_response
 
-  if (is_response_success(created_property_response)) {
+  if (is_response_success(created_item_response)) {
     # Successfully created the property
-    message("Successfully created item ", created_property_response$entity$id, " (", created_property_response$entity$labels$en$value, ")")
+    message("Successfully created item ", created_item_response$entity$id,
+            " (", created_item_response$entity$labels$en$value, ")")
 
     if (is.na(equivalence_property)) {
-    # Add the optional equivalence statement
-      wikidata_pid_df <- add_id_statement(
-        qid = created_property_response$entity$id,
+      # If equivalence must be handled, not rewritten yet
+      wikidata_qid_df <- add_id_statement(
+        qid = created_item_response$entity$id,
         pid = equivalence_property,
         o = equivalence_id,
         wikibase_api_url = wikibase_api_url,
@@ -153,18 +149,18 @@ create_property <- function(label,
     }
 
     return_dataframe <- data.frame(
-      action = "create_property",
-      id_on_target = created_property_response$entity$id,
+      action = "create_item",
+      id_on_target = created_item_response$entity$id,
       label = label,
-      description =  created_property_response$entity$descriptions[[1]]$value,
-      language =  created_property_response$entity$descriptions[[1]]$language,
-      datatype = created_property_response$entity$datatype,
+      description =  created_item_response$entity$descriptions[[1]]$value,
+      language =  created_item_response$entity$descriptions[[1]]$language,
+      datatype = "item",
       wikibase_api_url = wikibase_api_url,
       equivalence_property =  equivalence_property,
       equivalence_id = equivalence_id,
       success = TRUE,
       comment = NA_character_,
-      time = action_time,
+      time = action_timestamp,
       logfile = log_file_name
     )
 
@@ -176,33 +172,36 @@ create_property <- function(label,
 
     return_dataframe
   } else if (
-    "wikibase-validator-label-conflict" %in% unlist(created_property_response$error$messages)
-    ) {
+    # There is a wikibase-validator-...-conflict in the error message
+    any(
+      c( "wikibase-validator-label-conflict" %in% unlist(created_item_response$error$messages),
+         "wikibase-validator-label-with-description-conflict" %in% unlist(created_item_response$error$messages))
+    )) {
     # wikibase-validator-label-conflict: the property already exists, and this
     # information should be returned to the user.
-    message_strings <- unlist(created_property_response$error$messages)
+    message_strings <- unlist(created_item_response$error$messages)
     message(message_strings)
-    message_strings <- message_strings[which(grepl("Property:", message_strings))]
-    pattern <- "\\[\\[Property:*(.*?)\\|"
+    message_strings <- message_strings[which(grepl("Item:", message_strings))]
+    pattern <- "\\[\\[Item:*(.*?)\\|"
     result <- regmatches(message_strings, regexec(pattern, message_strings))
-    old_pid <- result[[1]][2]
+    old_qid <- result[[1]][2]
 
-    message("Problem with recording a new property with label='", label, "'.
-            A property already exists with this label: (", old_pid, ")")
+    message("Problem with recording a new item with label='", label, "'.
+            An item already exists with this label: (", old_qid, ")")
 
     return_dataframe <- data.frame(
-      action = "create_property",
-      id_on_target = old_pid,
+      action = "create_item",
+      id_on_target = old_qid,
       label = label,
       description = "<description match not inspected>",
       language =  language,
-      datatype = datatype,
+      datatype = "item",
       wikibase_api_url = wikibase_api_url,
       equivalence_property =  equivalence_property,
       equivalence_id = equivalence_id,
       success = FALSE,
       comment = "wikibase-validator-label-conflict, the label-language pair already exists.",
-      time = action_time,
+      time = action_timestamp,
       logfile = log_file_name
     )
 
@@ -217,22 +216,22 @@ create_property <- function(label,
 
     error_comments <- paste(
       unlist(
-        lapply(created_property_response$error$messages, function(x) x$name)
+        lapply(created_item_response$error$messages, function(x) x$name)
         ), collapse="|")
 
     return_dataframe <- data.frame(
-      action = "create_property",
+      action = "create_item",
       id_on_target = NA_character_,
       label = label,
       description = description,
       language =  language,
-      datatype = datatype,
+      datatype = "item",
       wikibase_api_url = wikibase_api_url,
       equivalence_property =  equivalence_property,
       equivalence_id = equivalence_id,
       success = FALSE,
       comment = error_comments,
-      time = action_time,
+      time = action_timestamp,
       logfile = log_file_name
     )
 
---
+++ R/item_identity_datastring_create.R
@@ -0,0 +1,32 @@
+#' @title Create a JSON for wbeditentity items
+#' @description
+#' Creates a JSON string of the label(s), descriptions(s) in various languages,
+#' adds the datatype. Required to define a property on Wikibase.
+#' @param labels_list A list property labels organised by language.
+#' @param descriptions_list A list of descriptions organised by language.
+#' @importFrom assertthat assert_that
+#' @importFrom jsonlite toJSON
+#' @return A JSON string for the wbeditentity API call
+#' @keywords internal
+
+item_identity_datastring_create <- function(labels_list,
+                                                descriptions_list) {
+  # Assert that the inputs are of correct types
+  assertthat::assert_that(
+    inherits(labels_list, "list"),
+    msg = "property_datastring_create(labels_list, ...) must be a list."
+  )
+
+  assertthat::assert_that(
+    inherits(descriptions_list, "list"),
+    msg = "property_datastring_create(descriptions_list, ...) must be a list."
+  )
+
+  jsonlite::toJSON(
+    list(
+      labels = labels_list,
+      descriptions = descriptions_list
+    ),
+    auto_unbox = T
+  )
+}
