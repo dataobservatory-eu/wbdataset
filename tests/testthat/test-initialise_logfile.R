f229eb48e97bccbc6a7be65494637bce166b351b
antaldaniel <antaldaniel@gmail.com>
2025-03-15 09:59:03 +0100

    remove initialise logfile

--- tests/testthat/test-initialise_logfile.R
+++
@@ -1,7 +0,0 @@
-test_that("initialise_logfile()", {
-  expect_s3_class(initialise_logfile(), "dataset_df")
-  expect_true(grepl(pattern="Wikibase Data Model Log", dataset_title(initialise_logfile())))
-  expect_equal(nrow(initialise_logfile()), 0)
-  expect_s3_class(initialise_logfile()$rowid, "haven_labelled_defined")
-})
-
